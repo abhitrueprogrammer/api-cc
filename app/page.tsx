faede851257daace1c4c5ac4d3fa6b604f9aabf6
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-09-21 20:28:25 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-Import React from  "react";
+"use client";
+import React, { useEffect, useState } from "react";
+interface Post {
+  id: number;
+  body: string;
+  title: string; // Add other properties if needed
+}
+
 export default function Home() {
-  let data = fetch('https://jsonplaceholder.typicode.com/posts')
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      let data = await res.json();
+      setPosts(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div>
-
+    <div className="m-10">
+      <div>Total Posts: {posts.length}</div>
+      
+      {posts.map((post) => (
+        post.id < 10?
+        <div className="m-10" key={post.id}>
+          {post.body}
+        </div>: <div></div>
+      ))}
     </div>
   );
 }
